b5cc5155d59dfe8fb3a780783fb48642aca0703b
Vincent Ekpanyaskun <vincent.ekp@gmail.com>
2024-11-24 21:46:18 +1100

    use scroll view for settings page

--- lib/page/settings_page.dart
+++ lib/page/settings_page.dart
@@ -7,7 +7,7 @@ class SettingsPage extends StatefulWidget {
   final int sfID;
 
   final SharedPreferences? prefs;
- 
+
   const SettingsPage({super.key, required this.prefs, required this.sfID});
 
   @override
@@ -16,8 +16,7 @@ class SettingsPage extends StatefulWidget {
 
 class _SettingsPageState extends State<SettingsPage> {
   bool prefLoaded = false;
-  
-  
+
   late String selectedKeyHarmony;
   late String selectedPlayingMode;
   late String selectedScale;
@@ -32,7 +31,8 @@ class _SettingsPageState extends State<SettingsPage> {
     selectedOctave = widget.prefs!.getString('octave')!;
     selectedScale = widget.prefs!.getString('currentScale')!;
     selectedPlayingMode = widget.prefs!.getString('playingMode')!;
-    selectedInstrument = Instrument.values.firstWhere((e) => e.name == widget.prefs!.getString('instrument')!);
+    selectedInstrument = Instrument.values
+        .firstWhere((e) => e.name == widget.prefs!.getString('instrument')!);
     selectedVisuals = widget.prefs!.getString('visuals')!;
     selectedSymbols = widget.prefs!.getString('symbols')!;
     print("Key Harmony: $selectedKeyHarmony");
@@ -57,151 +57,184 @@ class _SettingsPageState extends State<SettingsPage> {
       widget.prefs?.setString('instrument', selectedInstrument.name);
       widget.prefs?.setString('visuals', selectedVisuals);
       widget.prefs?.setString('symbols', selectedSymbols);
-      MidiPro().selectInstrument(sfId: widget.sfID, bank:selectedInstrument.bank, program: selectedInstrument.program);
+      MidiPro().selectInstrument(
+          sfId: widget.sfID,
+          bank: selectedInstrument.bank,
+          program: selectedInstrument.program);
       print('settings saved');
     });
   }
-  
-  @override
 
+  @override
   void initState() {
     super.initState();
     extractSettings();
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Settings'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            DropdownButtonFormField<Instrument>(
-              decoration: InputDecoration(labelText: 'Instrument'),
-              value: selectedInstrument,
-              items: Instrument.values
-                  .map((instrument) => DropdownMenuItem(
-                        value: instrument,
-                        child: Text(instrument.name),
-                      ))
-                  .toList(),
-              onChanged: (newValue) {
-                setState(() {
-                  selectedInstrument = newValue!;
-                });
-              },
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    DropdownButtonFormField<Instrument>(
+                      decoration: InputDecoration(labelText: 'Instrument'),
+                      value: selectedInstrument,
+                      items: Instrument.values
+                          .map((instrument) => DropdownMenuItem(
+                                value: instrument,
+                                child: Text(instrument.name),
+                              ))
+                          .toList(),
+                      onChanged: (newValue) {
+                        setState(() {
+                          selectedInstrument = newValue!;
+                        });
+                      },
+                    ),
+                    SizedBox(height: 16),
+                    DropdownButtonFormField<String>(
+                      decoration: InputDecoration(labelText: 'Key Centre'),
+                      value: selectedKeyHarmony,
+                      items: [
+                        'C',
+                        'C# / Db',
+                        'D',
+                        'D# / Eb',
+                        'E',
+                        'F',
+                        'F# / Gb',
+                        'G',
+                        'G# / Ab',
+                        'A',
+                        'A# / Bb',
+                        'B'
+                      ]
+                          .map((value) => DropdownMenuItem(
+                                value: value,
+                                child: Text(value),
+                              ))
+                          .toList(),
+                      onChanged: (newValue) {
+                        setState(() {
+                          selectedKeyHarmony = newValue!;
+                        });
+                      },
+                    ),
+                    SizedBox(height: 16),
+                    DropdownButtonFormField<String>(
+                      decoration: InputDecoration(labelText: 'Scale'),
+                      value: selectedScale,
+                      items: [
+                        'Major',
+                        'Minor',
+                        'Harmonic Minor',
+                        'Pentatonic Major',
+                        'Pentatonic Minor'
+                      ]
+                          .map((value) => DropdownMenuItem(
+                                value: value,
+                                child: Text(value),
+                              ))
+                          .toList(),
+                      onChanged: (newValue) {
+                        setState(() {
+                          selectedScale = newValue!;
+                        });
+                      },
+                    ),
+                    SizedBox(height: 16),
+                    DropdownButtonFormField<String>(
+                      decoration: InputDecoration(labelText: 'Octave'),
+                      value: selectedOctave,
+                      items: ['2', '3', '4', '5', '6', '7']
+                          .map((value) => DropdownMenuItem(
+                                value: value,
+                                child: Text(value),
+                              ))
+                          .toList(),
+                      onChanged: (newValue) {
+                        setState(() {
+                          selectedOctave = newValue!;
+                        });
+                      },
+                    ),
+                    SizedBox(height: 16),
+                    DropdownButtonFormField<String>(
+                      decoration: InputDecoration(labelText: 'Keyboard Mode'),
+                      value: selectedPlayingMode,
+                      items: ['Single Note', 'Triad Chord', 'Power Chord']
+                          .map((value) => DropdownMenuItem(
+                                value: value,
+                                child: Text(value),
+                              ))
+                          .toList(),
+                      onChanged: (newValue) {
+                        setState(() {
+                          selectedPlayingMode = newValue!;
+                        });
+                      },
+                    ),
+                    SizedBox(height: 16),
+                    DropdownButtonFormField<String>(
+                      decoration: InputDecoration(labelText: 'Visuals'),
+                      value: selectedVisuals,
+                      items: ['Grid', 'Custom']
+                          .map((value) => DropdownMenuItem(
+                                value: value,
+                                child: Text(value),
+                              ))
+                          .toList(),
+                      onChanged: (newValue) {
+                        setState(() {
+                          selectedVisuals = newValue!;
+                        });
+                      },
+                    ),
+                    SizedBox(height: 16),
+                    DropdownButtonFormField<String>(
+                      decoration:
+                          InputDecoration(labelText: 'Keyboard Symbols'),
+                      value: selectedSymbols,
+                      items: ['Shapes', 'Letters', 'Numbers', 'None']
+                          .map((value) => DropdownMenuItem(
+                                value: value,
+                                child: Text(value),
+                              ))
+                          .toList(),
+                      onChanged: (newValue) {
+                        setState(() {
+                          selectedSymbols = newValue!;
+                        });
+                      },
+                    ),
+                    SizedBox(height: 32),
+                  ],
+                ),
+              ),
             ),
-            SizedBox(height: 16),
-            DropdownButtonFormField<String>(
-              decoration: InputDecoration(labelText: 'Key Centre'),
-              value: selectedKeyHarmony,
-              items: ['C', 'C# / Db', 'D', 'D# / Eb', 'E', 'F', 'F# / Gb', 'G', 'G# / Ab', 'A', 'A# / Bb', 'B']
-                  .map((value) => DropdownMenuItem(
-                        value: value,
-                        child: Text(value),
-                      ))
-                  .toList(),
-              onChanged: (newValue) {
-                setState(() {
-                  selectedKeyHarmony = newValue!;
-                });
-              },
-            ),
-            SizedBox(height: 16),
-            DropdownButtonFormField<String>(
-              decoration: InputDecoration(labelText: 'Scale'),
-              value: selectedScale,
-              items: ['Major', 'Minor', 'Harmonic Minor', 'Pentatonic Major', 'Pentatonic Minor']
-                  .map((value) => DropdownMenuItem(
-                        value: value,
-                        child: Text(value),
-                      ))
-                  .toList(),
-              onChanged: (newValue) {
-                setState(() {
-                  selectedScale = newValue!;
-                });
-              },
-            ),
-            SizedBox(height: 16),
-            DropdownButtonFormField<String>(
-              decoration: InputDecoration(labelText: 'Octave'),
-              value: selectedOctave,
-              items: ['2', '3', '4', '5', '6', '7']
-                  .map((value) => DropdownMenuItem(
-                        value: value,
-                        child: Text(value),
-                      ))
-                  .toList(),
-              onChanged: (newValue) {
-                setState(() {
-                  selectedOctave = newValue!;
-                });
-              },
-            ),
-            SizedBox(height: 16),
-            DropdownButtonFormField<String>(
-              decoration: InputDecoration(labelText: 'Keyboard Mode'),
-              value: selectedPlayingMode,
-              items: ['Single Note', 'Triad Chord', 'Power Chord']
-                  .map((value) => DropdownMenuItem(
-                        value: value,
-                        child: Text(value),
-                      ))
-                  .toList(),
-              onChanged: (newValue) {
-                setState(() {
-                  selectedPlayingMode = newValue!;
-                });
-              },
-            ),
-            SizedBox(height: 16),
-            DropdownButtonFormField<String>(
-              decoration: InputDecoration(labelText: 'Visuals'),
-              value: selectedVisuals,
-              items: ['Grid', 'Custom']
-                  .map((value) => DropdownMenuItem(
-                        value: value,
-                        child: Text(value),
-                      ))
-                  .toList(),
-              onChanged: (newValue) {
-                setState(() {
-                  selectedVisuals = newValue!;
-                });
-              },
-            ),
-            SizedBox(height: 16),
-            DropdownButtonFormField<String>(
-              decoration: InputDecoration(labelText: 'Keyboard Symbols'),
-              value: selectedSymbols,
-              items: ['Shapes', 'Letters', 'Numbers', 'None']
-                  .map((value) => DropdownMenuItem(
-                        value: value,
-                        child: Text(value),
-                      ))
-                  .toList(),
-              onChanged: (newValue) {
-                setState(() {
-                  selectedSymbols = newValue!;
-                });
-              },
-            ),
-            SizedBox(height: 32),
-            ElevatedButton(
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ElevatedButton(
               onPressed: () {
                 saveSettings();
                 Navigator.pop(context);
               },
               child: Text('Save Settings'),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
 }
-
