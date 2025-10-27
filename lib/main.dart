5a08f7a54a01e3dc70be9bd6ce3b44f7817c31ef
Vincent Ekpanyaskun <vincent.ekp@gmail.com>
2025-10-27 19:30:56 +1100

    update app name, bundle ID and icon

--- lib/main.dart
+++ lib/main.dart
@@ -376,7 +376,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'TadBuddy',
+      title: 'GuitarApp',
       debugShowCheckedModeBanner: false, // Removes the debug banner
       home: Builder(
         builder: (context) {
